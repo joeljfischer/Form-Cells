b517738579da3de15335baf666ecc9228bd852ba
Joel Fischer <joeljfischer@gmail.com>
2018-11-17 18:07:56 -0500

    Initial Commit

---
+++ FormCells/FormCells.h
@@ -0,0 +1,19 @@
+//
+//  FormCells.h
+//  FormCells
+//
+//  Created by Joel Fischer on 11/17/18.
+//  Copyright © 2018 Joel Fischer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FormCells.
+FOUNDATION_EXPORT double FormCellsVersionNumber;
+
+//! Project version string for FormCells.
+FOUNDATION_EXPORT const unsigned char FormCellsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormCells/PublicHeader.h>
+
+
