af542e40e48316cf32f3b46946617cdb67e516cc
Joel Fischer <joeljfischer@gmail.com>
2019-02-10 21:43:44 -0500

    Add basic cell

---
+++ FormCells/Form Cells/Basic Tap Cell/BasicTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  BasicTableViewCell.swift
+//  Ding-Timer
+//
+//  Created by Joel Fischer on 2/10/19.
+//  Copyright © 2019 Joel Fischer. All rights reserved.
+//
+
+import UIKit
+
+class BasicTableViewCell: UITableViewCell, FormTapCallbackable {
+    var onSelection: ((FormTapCallbackable) -> Void)?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
+
+extension BasicTableViewCell: FormTappable {
+    func tapped() {
+        self.onSelection?(self)
+    }
+}
