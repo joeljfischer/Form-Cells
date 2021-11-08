add9aad0db1f2239c00727032b7f28d5a1fec48e
Joel Fischer <joeljfischer@gmail.com>
2021-11-08 08:18:35 -0500

    Update basic cell

--- Form Cells/Tap Cell/Basic Cell/BasicTableViewCell.swift
+++ Form Cells/Tap Cell/Basic Cell/BasicTableViewCell.swift
@@ -1,26 +1,13 @@
 //
 //  BasicTableViewCell.swift
-//  Ding-Timer
+//  FormCells
 //
-//  Created by Joel Fischer on 2/10/19.
-//  Copyright © 2019 Joel Fischer. All rights reserved.
+//  Created by Joel Fischer on 11/8/21.
+//  Copyright © 2021 Joel Fischer. All rights reserved.
 //
 
 import UIKit
 
 class BasicTableViewCell: FormTableViewCell<Any> {
-    @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var iconToTitleConstraint: NSLayoutConstraint!
-    @IBOutlet weak var leadingToTitleConstraint: NSLayoutConstraint!
-
-    override func awakeFromNib() {
-        if iconImageView.image != nil {
-            iconToTitleConstraint.isActive = false
-            leadingToTitleConstraint.isActive = true
-        } else {
-            iconToTitleConstraint.isActive = true
-            leadingToTitleConstraint.isActive = false
-        }
-    }
 }
---
+++ Form Cells/Tap Cell/Basic Cell/BasicWithImageTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BasicTableViewCell.swift
+//  Ding-Timer
+//
+//  Created by Joel Fischer on 2/10/19.
+//  Copyright © 2019 Joel Fischer. All rights reserved.
+//
+
+import UIKit
+
+class BasicWithImageTableViewCell: FormTableViewCell<Any> {
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+}
